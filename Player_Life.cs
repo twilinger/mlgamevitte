6980414892245c2cbd0df4f4325a9ac0d72d10f3
twilinger <rafaelasdeveloper@gmail.com>
2023-06-06 02:20:49 +0300

    some respawn changes

---
+++ Player_Life.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Life : MonoBehaviour
+{
+    private Animator anim;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Debug.Log("Player went into the trap");
+            PlayerDeath();
+        }
+    }
+    private void PlayerDeath()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("Dead");
+
+    }
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
